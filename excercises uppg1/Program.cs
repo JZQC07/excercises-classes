858fa3f0ff426a48da718fa9cea9352ebce42040
Jonatan Schultz <jonatan_schultz@hotmail.com>
2019-09-26 17:29:05 +0200

    added gitignore

---
+++ excercises uppg1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace excercises_uppg1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Worlasfsafd!");
+        }
+    }
+}
