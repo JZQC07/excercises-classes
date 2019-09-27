670e417f296dac3ea0071c8c314f52cae178d3da
Jonatan Schultz <jonatan_schultz@hotmail.com>
2019-09-27 09:02:40 +0200

    figo barks

--- excercises uppg1/Program.cs
+++ excercises uppg1/Program.cs
@@ -6,7 +6,29 @@ namespace excercises_uppg1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Worlasfsafd!");
+            Console.WriteLine("Hello World!");
+            dog.Bark();
         }
+        
+    }
+    class dog
+    {
+        private string name = "Figo";
+        private int age = 2;
+        private string race = "Labrador";
+
+        public static void Bark()
+        {
+            Console.WriteLine("Figo Barks");
+        }
+        public static void Fetch()
+        {   
+            Console.WriteLine("Figo is only two years old! He loves to play fetch.");
+        }
+        public static void eat()
+        {
+            Console.WriteLine("Figo is a {0}, he eats a lot!");
+        }
+
     }
 }
